b85ef9ae0a2a90f1e341572bdba12a1a74f926cc
Björn Plüster <bjoern.pl@outlook.de>
2021-01-01 22:18:38 +0100

    Changed loading to handle caching for multiple directories

--- data/load.py
+++ data/load.py
@@ -1,12 +1,14 @@
 import json
 from pathlib import Path
 from typing import Tuple, Dict
+import hashlib
 
 
 class Handler:
 
     def __init__(self, path):
-        self._cache_path = Path("cache") / str(hash(path))
+        path_hash = str(hashlib.md5(str(path.resolve()).encode('utf-8')).hexdigest())
+        self._cache_path = Path("cache") / path_hash
         print(self._cache_path.absolute())
         if not self._cache_path.exists():
             self._cache_path.mkdir(parents=True)
@@ -14,20 +16,30 @@ class Handler:
         self.cache_file = self._cache_path / "cache.json"
         self._init_files()
 
-    def _init_files(self):
-        if not self.stats_file.exists():
-            with open(self.stats_file, 'w') as fp:
+    @staticmethod
+    def _create_if_not_exists(file):
+        if not file.exists():
+            with open(file, 'w') as fp:
                 json.dump({}, fp)
 
-        if not self.cache_file.exists():
-            with open(self.cache_file, 'w') as fp:
-                json.dump({}, fp)
+    def _init_files(self):
+        self._create_if_not_exists(self.stats_file)
+        self._create_if_not_exists(self.cache_file)
+
+    @staticmethod
+    def _read_json(file):
+        with open(file) as f:
+            return json.load(f)
+
+    @staticmethod
+    def _write_json(obj, file):
+        if obj:
+            with open(file, 'w') as f:
+                json.dump(obj, f, indent=4)
 
     def _load_data(self) -> Tuple[Dict, Dict]:
-        with open(self.stats_file) as f:
-            stats = json.load(f)
-        with open(self.cache_file) as f:
-            cache = json.load(f)
+        stats = self._read_json(self.stats_file)
+        cache = self._read_json(self.cache_file)
         return stats, cache
 
     def load(self) -> Tuple[Dict, Dict]:
@@ -38,8 +50,5 @@ class Handler:
         return self._load_data()
 
     def save(self, stats=None, cache=None):
-        if stats:
-            json.dumps(stats, fp=self.stats_file)
-
-        if cache:
-            json.dumps(cache, fp=self.cache_file)
+        self._write_json(stats, self.stats_file)
+        self._write_json(cache, self.cache_file)
